df8c06a89c79c6a7b8864b251a2460357e7c4498
Nuriddin <nuriddinyu@synel.co.il>
2022-02-13 20:03:31 +0500

    added service for todo app

---
+++ src/app/app.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  public constructor() {}
+
+  public getAllTodos(): void {
+  }
+
+  public addTodo(): void {
+  }
+
+  public editTodo(): void {
+  }
+
+  public deleteTodo(): void {
+  }
+
+  public toggleStatusTodo(): void {
+  }
+
+  public searchTodos(): void {
+  }
+
+}
